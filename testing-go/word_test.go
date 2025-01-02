a02a0f32218ab6522713b8e6d25890df2ea7643b
nitishsharma2825 <sharnit2825@gmail.com>
2025-01-02 23:24:37 +0530

    random testing

--- testing-go/word_test.go
+++ testing-go/word_test.go
@@ -1,6 +1,32 @@
 package word
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // random rune up to '\u0999'
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
 
 func TestPalindrome(t *testing.T) {
 	// Table driven testing
