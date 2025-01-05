c0dc8b1c95262ff77de6c49bd139833ec2550482
nitishsharma2825 <sharnit2825@gmail.com>
2025-01-05 12:13:06 +0530

    server streaming

---
+++ grpc-demo-yt/server/server_stream.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	pb "grpc-demo-yt/proto"
+	"log"
+	"time"
+)
+
+func (s *helloServer) callSayHelloServerStream(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
+	log.Printf("got request with names: %v", req.Names)
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello" + name,
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+		time.Sleep(2 * time.Second)
+	}
+	return nil
+}
